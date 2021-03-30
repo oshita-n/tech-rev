0245537ccd9e969cb1142292b78f392bd306cbdb
oshita-n <oshita-n@whispon.com>
2021-03-31 02:00:58 +0900

    editorの変更

--- tech-rev-app/pages/edit.tsx
+++ tech-rev-app/pages/edit.tsx
@@ -1,27 +1,42 @@
-import Link from 'next/link'
-import 'codemirror/lib/codemirror.css';
-import '@toast-ui/editor/dist/toastui-editor.css';
- 
-import { Editor } from '@toast-ui/react-editor';
+import React, { useEffect, useRef, useState } from 'react'
+import {Editor, EditorState, RichUtils} from 'draft-js';
+import 'draft-js/dist/Draft.css'
 
+const DraftJSEditor = () => {
+  const [editorEnable, setEditorEnable] = useState(false)
+  const [editorState, setEditorState] = useState(() => 
+    EditorState.createEmpty()
+  );
+  const editorRef = useRef<Editor>(null)
 
-const edit = () => {
-    return (
-        <div>
+  useEffect(() => {
+    setEditorEnable(true)
+  }, [])
+
+  const handleBoldClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault()
+    setEditorState(RichUtils.toggleInlineStyle(editorState, "BOLD"))
+  }
+
+  return (
+    <div>
+        <h1 className="text-center text-3xl pt-10">記事を書く</h1>
+        <div onClick={() => {editorRef.current?.focus()}}>
+        {editorEnable && (
+            <div className="p-10">
+            <button className="bg-gray-100" onMouseDown={handleBoldClick}>Bold</button>
             <Editor
-                initialValue=""
-                previewStyle="vertical"
-                height="600px"
-                initialEditType="markdown"
-                useCommandShortcut={true}
+            ref={editorRef}
+            placeholder="文字を入力してください"
+            editorKey="test-key"
+            editorState={editorState}
+            onChange={setEditorState}
             />
-            <div className="grid grid-cols-2 gap-4">
-                <button className="bg-yellow-300  hover:bg-yellow-200">投稿</button>
-                <button className="bg-yellow-300  hover:bg-yellow-200">下書き</button>
             </div>
-            <Link href="/"><a className="text-3xl pl-2 bg-blue-100">ホームに戻る</a></Link>
+        )}
         </div>
-    );
-};
+    </div>
+  );
+}
 
-export default edit;
+export default DraftJSEditor
