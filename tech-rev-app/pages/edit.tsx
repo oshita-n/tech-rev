98f8885a36a7a4a70be9acfa39cefb8c02ea1435
oshita-n <oshita-n@whispon.com>
2021-03-30 21:24:58 +0900

    ページの追加

---
+++ tech-rev-app/pages/edit.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const edit = () => {
+    return (
+        <div>
+            <p class="text-6xl">Editorを追加予定。</p>
+            <Link href="/"><a class="text-3xl">ホームに戻る</a></Link>
+        </div>
+    );
+};
+export default edit;
--- tech-rev-app/pages/index.tsx
+++ tech-rev-app/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -10,27 +11,27 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 class="text-center text-8xl">
           TechRev!
         </h1>
 
-        <p className={styles.description}>
+        <p class="text-center text-3xl pb-40">
           書いた記事をレビューしあって、記事の品質を高めましょう
         </p>
-
+        
         <div class="grid grid-cols-2 gap-4 ">
-          <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-md cursor-pointer hover:bg-gray-200 space-y-10">
-            <blockquote>記事執筆</blockquote>
-            <figcaption class="font-medium">
-              <div class="text-gray-500">
+          <Link href="/edit">
+            <div class="max-w-sm mx-auto p-6 bg-gray-300 rounded-lg shadow-md cursor-pointer hover:bg-gray-200">
+            <h1 class="text-center text-2xl">記事執筆</h1>
+              <h2 class="text-gray-500">
                 記事を書いて知識を共有しましょう
-              </div>
-            </figcaption>
-          </div>
+              </h2>
+            </div>
+          </Link>
 
-          <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-md cursor-pointer hover:bg-gray-200">
-            <p>レビューする</p>
-            <p>レビューして記事の品質を高めましょう</p>
+          <div class="max-w-sm mx-auto p-6 bg-gray-300 rounded-lg shadow-md cursor-pointer hover:bg-gray-200">
+            <h1 class="text-center text-2xl">レビューする</h1>
+            <h2 class="text-gray-500">レビューして記事の品質を高めましょう</h2>
           </div>
         </div>
       </main>
